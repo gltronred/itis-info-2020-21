1a2b494a33793f4314cccaf2d7bae2a44127b7a2
Mansur Ziiatdinov <gltronred@gmail.com>
2020-09-08 15:02:36 +0300

    Комментарии к L1

--- 1/L1.java
+++ 1/L1.java
@@ -8,18 +8,21 @@ public class L1 {
         System.out.println("world!");
         System.out.println(345);
 
+        // Выражения
         System.out.println(2*2);
         System.out.println(10/(2-1+1));
         System.out.println(9%2);  // 1 - остаток
         System.out.println(9/2);  // 4
         System.out.println(9.0/2);// 4.5
 
+        // Переменные
         int a = 1;
         int b = a + 3; // 4
         int c;
         c = b * (a + a);
         System.out.println(c);
 
+        // Типы переменных и неявные преобразования
         double f = 1.5;
         f = c;
         System.out.println(f);
