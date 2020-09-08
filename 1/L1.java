7ff341cd88272eab684a85af52b34c680738ab0e
Mansur Ziiatdinov <gltronred@gmail.com>
2020-09-08 14:59:23 +0300

    Пример L1

---
+++ 1/L1.java
@@ -0,0 +1,27 @@
+
+
+public class L1 {
+    public static void main(String[] args) {
+        // print - без переноса
+        System.out.print("Hello ");
+        // println - с переносом
+        System.out.println("world!");
+        System.out.println(345);
+
+        System.out.println(2*2);
+        System.out.println(10/(2-1+1));
+        System.out.println(9%2);  // 1 - остаток
+        System.out.println(9/2);  // 4
+        System.out.println(9.0/2);// 4.5
+
+        int a = 1;
+        int b = a + 3; // 4
+        int c;
+        c = b * (a + a);
+        System.out.println(c);
+
+        double f = 1.5;
+        f = c;
+        System.out.println(f);
+    }
+}
