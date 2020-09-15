645ae921fe9065cbace96167bc84a75aeb44485a
Mansur Ziiatdinov <gltronred@gmail.com>
2020-09-15 14:57:50 +0300

    Проверка условия "существует ..." и др.комментарии

--- 3/L3.java
+++ 3/L3.java
@@ -5,20 +5,43 @@ public class L3 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
+        // Проверка условия "для всех ..."
+        // F(n) = forall a in {2,...,n-1} (a не делит n)
+        // F(n) = n - простое
+        //
         // f - для всех проверенных a,
         //     a не делит n
         boolean f = true;
         int a = 2;
-        while (a < n) {
+        while (a < n && f) {
             // f = f && (n % a != 0); // обновили f
             f &= (n % a != 0);
             a++;
         }
 
+        // Проверка условия "существует ..."
+        // G(n) = exists a in {2,...,n-1} (a делит n)
+        // G(n) = n - составное
+        //
+        // g - существует проверенное b,
+        //     b делит n
+        boolean g = false;
+        int b = 2;
+        while (b < n && !g) {
+            // g = g || (n % b == 0);
+            g |= (n % b == 0);
+            b++;
+        }
+
         if (f) {
-            System.out.println("YES"); // n - простое
+            System.out.println("YES"); // F(n)
         } else {
-            System.out.println("NO"); // n - составное
+            System.out.println("NO"); // не F(n)
+        }
+        if (g) {
+            System.out.println("YES"); // G(n)
+        } else {
+            System.out.println("NO"); // не G(n)
         }
     }
 }
