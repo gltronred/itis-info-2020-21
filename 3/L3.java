0d3ce3380bc358641451265a745602d952e1d313
Mansur Ziiatdinov <gltronred@gmail.com>
2020-09-15 14:34:53 +0300

    Проверка на простоту - 1

--- 3/L3.java
+++ 3/L3.java
@@ -4,6 +4,22 @@ public class L3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println(n);
+
+        // f - для всех проверенных a,
+        //     a не делит n
+        boolean f = true;
+        int a = 2;
+        while (a < n) {
+            if (n % a == 0) {
+                f = false;
+            }
+            a++;
+        }
+
+        if (f) {
+            System.out.println("YES"); // n - простое
+        } else {
+            System.out.println("NO"); // n - составное
+        }
     }
 }
