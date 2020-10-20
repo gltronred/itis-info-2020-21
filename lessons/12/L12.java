182b6be51ac7a827c1f264fc46123bfa8e8fb420
Mansur Ziiatdinov <gltronred@gmail.com>
2020-10-20 15:08:40 +0300

    Solution: cyclic shift

--- lessons/12/L12.java
+++ lessons/12/L12.java
@@ -1,8 +1,22 @@
 import java.util.*;
 
 public class L12 {
+    // Циклический сдвиг массива a влево на k позиций
+    // Меняется передаваемый массив a
     public static void cyclicShift(int[] a, int k) {
-        // Implement me!!!
+        int n = a.length;
+        k %= n; // чтобы сдвиг всегда был меньше длины массива
+                // иначе - ArrayIndexOutOfBounds в первом цикле
+        int[] t = new int[k];
+        for (int i = 0; i < k; i++) {
+            t[i] = a[i];
+        }
+        for (int i = 0; i < n - k; i++) {
+            a[i] = a[i+k];
+        }
+        for (int i = 0; i < k; i++) {
+            a[n - k + i] = t[i];
+        }
     }
     public static void main(String[] args) {
         int[] a = new int[10];
