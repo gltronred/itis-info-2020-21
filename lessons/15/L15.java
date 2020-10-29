8dbfb016fb5c884f15ced8eea58ac4cc5a4451a2
Mansur Ziiatdinov <gltronred@gmail.com>
2020-10-29 12:20:38 +0300

    Проверка, что правильный ответ правильный

--- lessons/15/L15.java
+++ lessons/15/L15.java
@@ -12,14 +12,22 @@ public class L15 {
         test(new int[]{1,2,3,3,4,4,5,7,8},6,-1);
         test(new int[]{1,2,3,3,4,4,5,7,8},1,0);
         test(new int[]{1,2,3,3,4,4,5,7,8},3,2);
+        test(new int[]{1,2,3,3,4,4,5,7,8},3,3);
         test(new int[]{1,2,3,3,4,4,5,7,8},8,8);
         test(new int[]{1,2,3,3,4,4,5,7,8},-1,-1);
         test(new int[]{1,2,3,3,4,4,5,7,8},10,-1);
     }
     public static void test(int[] a, int x, int r) {
+        if (r != -1 && a[r] != x) {
+            System.out.print("FAIL! " + Arrays.toString(a) + "; r=" + r);
+            if (r != -1) {
+                System.out.print("; a[" + r + "]=" + a[r]);
+            }
+            System.out.println("; x=" + x);
+        }
         int i = find(a,x);
         if (i == -1 && r == -1 ||
-            i != -1 && a[i] == x && a[r] == x) {
+            i != -1 && a[i] == x) {
             System.out.print("OK");
         } else {
             System.out.print("WA");
