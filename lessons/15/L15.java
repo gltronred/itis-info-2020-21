efb7c0590d19e090172a77bf990cd4e0798efdc1
Mansur Ziiatdinov <gltronred@gmail.com>
2020-10-29 12:06:56 +0300

    Заготовка бинарного поиска

---
+++ lessons/15/L15.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class L15 {
+    public static int find(int[] a, int x) {
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] a = new int[]{1,2,3,3,4,4,5,7,8};
+        System.out.println(find(a,6));
+    }
+}
