6e2496a1258fc70d4ac3e27923d776188ee9fb51
Mansur Ziiatdinov <gltronred@gmail.com>
2020-11-05 12:55:05 +0300

    Add legs to Chair

--- lessons/16/Chair.java
+++ lessons/16/Chair.java
@@ -2,31 +2,41 @@
 // Название класса должно начинаться с заглавной буквы
 // и совпадать с названием файла
 public class Chair {
-    // Поле для хранения количества ног
-    int legsCount;
+    // Поле для хранения ног
+    Leg[] legs = new Leg[5];
+    boolean back;
+    int armRests;
 
     // Конструктор
     //
     // Если без него - будет только конструктор по умолчанию
     // без параметров Chair()
     public Chair(int legs) {
+        // this - конкретно этот объект, с которым работаем
+        this.legs = new Leg[legs];
+        for (int i = 0; i<legs; i++) {
+            this.legs[i] = new Leg();
+        }
         System.out.println("Created a chair with " + legs + " legs");
-        legsCount = legs;
     }
 
     public void addLeg() {
-        legsCount++;
+        // legsCount++;
     }
 
     public void addMoreLegs(int additionalLegs) {
-        legsCount += additionalLegs;
+        // legsCount += additionalLegs;
     }
 
-    public int getLegs() { return legsCount; }
+    public int getLegs() { return legs.length; }
 
     // Преобразование в строку (для System.out.println)
     public String toString() {
-        return getName() + " with " + legsCount + " legs";
+        String s = "";
+        for (int i=0; i<getLegs(); i++) {
+            s += (legs[i] + " ");
+        }
+        return getName() + " with " + getLegs() + " legs: " + s;
     }
 
     // Статический метод - в классе,
