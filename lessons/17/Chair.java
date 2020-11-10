721ec0eefd56d00dfa8382d419e232c6241e96e7
Mansur Ziiatdinov <gltronred@gmail.com>
2020-11-10 14:20:51 +0300

    Lesson 17

---
+++ lessons/17/Chair.java
@@ -0,0 +1,51 @@
+
+// Название класса должно начинаться с заглавной буквы
+// и совпадать с названием файла
+public class Chair {
+    // Поле для хранения ног
+    Leg[] legs = new Leg[5];
+    boolean back;
+    static int armRests;
+
+    // Конструктор
+    //
+    // Если без него - будет только конструктор по умолчанию
+    // без параметров Chair()
+    public Chair(int legs) {
+        // this - конкретно этот объект, с которым работаем
+        this.legs = new Leg[legs];
+        for (int i = 0; i<legs; i++) {
+            this.legs[i] = new Leg();
+        }
+        System.out.println("Created a chair with " + legs + " legs");
+    }
+
+    public void addLeg() {
+        // legsCount++;
+    }
+
+    public void addMoreLegs(int additionalLegs) {
+        // legsCount += additionalLegs;
+    }
+
+    public int getLegs() { return legs.length; }
+
+    // Преобразование в строку (для System.out.println)
+    public String toString() {
+        String s = "";
+        for (int i=0; i<getLegs(); i++) {
+            s += (legs[i] + " ");
+        }
+        return getName() + " with " + getLegs() + " legs: " + s;
+    }
+
+    // Статический метод - в классе,
+    // для всех объектов класса один и тот же
+    public static String getName() {
+        return "A chair with " + armRests + " arm rests";
+    }
+
+    public static void setArmRests(int arms) {
+        armRests = arms;
+    }
+}
---
+++ lessons/17/L17.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class L17 {
+    public static void main(String[] args) {
+       Chair c1 = new Chair(3);
+       System.out.println(c1);
+    }
+}
