344140170a6948b3ee3571d8b5c9f05e35a7aba0
Mansur Ziiatdinov <gltronred@gmail.com>
2020-11-10 14:42:28 +0300

    Add color to chair

--- lessons/17/ColorChair.java
+++ lessons/17/ColorChair.java
@@ -3,4 +3,25 @@
 //
 // Наследуемся от обычного кресла
 public class ColorChair extends Chair {
+    int red;
+    int green;
+    int blue;
+
+    public void setColor(int r, int g, int b) {
+        red = r;
+        green = g;
+        blue = b;
+    }
+
+    public String getColor() {
+        if (red == 255 && green == 0 && blue == 0)
+            return "red";
+        if (red == 0 && green == 255 && blue == 0)
+            return "green";
+        if (red == 0 && green == 0 && blue == 255)
+            return "blue";
+        if (red == 255 && green == 255 && blue == 255)
+            return "white";
+        return "unknown";
+    }
 }
--- lessons/17/L17.java
+++ lessons/17/L17.java
@@ -2,9 +2,11 @@ import java.util.*;
 
 public class L17 {
     public static void main(String[] args) {
-       Chair c1 = new Chair(3);
+       Chair c1 = new Chair(5);
        System.out.println(c1);
        ColorChair c2 = new ColorChair();
        System.out.println(c2);
+       c2.setColor(255,255,255);
+       System.out.println("Color of c2: " + c2.getColor());
     }
 }
