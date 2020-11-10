93b28a982b57e8cdaef0165e97bf0e5707e1fddd
Mansur Ziiatdinov <gltronred@gmail.com>
2020-11-10 15:29:28 +0300

    Полиморфизм

--- lessons/17/L17.java
+++ lessons/17/L17.java
@@ -4,10 +4,10 @@ public class L17 {
     public static void main(String[] args) {
        // Chair c1 = new Chair(5);
        // System.out.println(c1);
-       ColorChair c2 = new ColorChair(255,255,255);
+       Chair c2 = new ColorChair(255,255,255);
        System.out.println(c2);
        // c2.setColor(255,255,255);
-       System.out.println("Color of c2: " + c2.getColor());
+       // System.out.println("Color of c2: " + c2.getColor());
        System.out.println("Legs: " + c2.getLegs());
     }
 }
