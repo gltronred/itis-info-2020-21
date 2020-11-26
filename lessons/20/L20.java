1697e73133744183fe69f4f3152054caee467a49
Mansur Ziiatdinov <gltronred@gmail.com>
2020-11-26 12:34:58 +0300

    Comments

--- lessons/20/L20.java
+++ lessons/20/L20.java
@@ -11,8 +11,8 @@ public class L20 {
             " ",
             "",
             "Thisisoneword",
-            "Hello\u200bworld!",
-            "Hello\u200cworld!",
+            "Hello\u200bworld!", // one - \u200b is zero-width joiner
+            "Hello\u200cworld!", // one
             "Helló, világ - magyar", // three, if you check that word contains letters :)
             "One\u00a0two\u205fthree\u2000four" // three words - because Java thinks that \u00a0 (NBSP) is not whitespace
         };
