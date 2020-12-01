d3dbb2cc4086ddc7a2ca4fd518a9a3ea877b2b33
Mansur Ziiatdinov <gltronred@gmail.com>
2020-12-01 14:37:06 +0300

    More regexes

--- lessons/21/L21.java
+++ lessons/21/L21.java
@@ -3,8 +3,27 @@ import java.util.regex.*;
 
 public class L21 {
     public static void main(String[] args) {
-        Pattern p = Pattern.compile("Go*al");
-        Matcher m = p.matcher("Gooooooal");
-        System.out.println(m.matches());
+        // Pattern p = Pattern.compile("([Gg]\\.)+al!?|No");
+        // Pattern p = Pattern.compile(".*\\bh.*");
+        Pattern p = Pattern.compile("(g[o.]){2,3}");
+        String[] arr = new String[]{
+                "GoGoGoal!",
+                "No",
+                "gogogoal",
+                "gugugual",
+                "gog.gogogo",
+                "gog.go",
+                "gogo",
+                "go",
+                "g.G.g.al",
+                "hello    world",
+                "   hello world",
+                "world,hello",
+                "world\nhello"
+        };
+        for (String s : arr) {
+            Matcher m = p.matcher(s);
+            System.out.println(s + ": " + m.matches());
+        }
     }
 }
