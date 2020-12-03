90f5b3ca42006a637609860b734f16d87ef429bd
Mansur Ziiatdinov <gltronred@gmail.com>
2020-12-03 12:57:31 +0300

    Именованные группы

--- lessons/22/L22.java
+++ lessons/22/L22.java
@@ -3,9 +3,9 @@ import java.util.regex.*;
 
 public class L22 {
     public static void main(String[] args){
-        Pattern p = Pattern.compile("(\\d{4}) ?г\\.");
+        Pattern p = Pattern.compile("(?<year>(\\d){4})(((?: )?)г\\.)");
         String[] arr = {
-            "Прошлый, 2020 г., был непростой, а составной",
+            "Прошлый, 2020 г., был не простой, а составной",
             "Документ составлен 1 марта 1995г.",
             "В 2010г. было высажено 1234 гриба, а в 2011 г. - 231 куст",
             "А здесь года нет"
@@ -16,7 +16,10 @@ public class L22 {
             boolean b = false;
             while (m.find()) {
                 b = true;
-                System.out.println(m.group(1));
+                for (int g = 0; g <= m.groupCount(); g++) {
+                    System.out.println(g + " = \"" + m.group(g) + "\"");
+                }
+                System.out.println("year = " + m.group("year"));
             }
             if (!b) {
                 System.out.println("NO MATCH");
